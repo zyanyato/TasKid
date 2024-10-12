80ed7ae27bbe09ec70ab27c212629ea128056d1e
zyanyato <zyannya.to@gmail.com>
2024-10-12 14:39:32 -0600

    data

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 
 const sequelize = process.env.DB_URL ? new Sequelize(process.env.DB_URL) : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'postgres',
     port: process.env.DB_PORT,
     dialectOptions: {
